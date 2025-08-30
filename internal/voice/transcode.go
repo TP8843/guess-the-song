daad176643992921fed58457032fb687f8c14941
Thomas Parr <github.us8r1@tpalias.com>
2025-08-30 19:48:40 +0100

    Add volume adjustment to transcode to reduce volume

--- internal/voice/transcode.go
+++ internal/voice/transcode.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"encoding/binary"
 	"errors"
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -19,15 +20,16 @@ import (
 // a lot of other problems that are not handled well at this time.
 // These below values seem to provide the best overall performance
 const (
-	channels  int = 2                   // 1 for mono, 2 for stereo
-	frameRate int = 48000               // audio sampling rate
-	frameSize int = 960                 // uint16 size of each audio frame
-	maxBytes  int = (frameSize * 2) * 2 // max size of opus data
+	channels  int     = 2                   // 1 for mono, 2 for stereo
+	frameRate int     = 48000               // audio sampling rate
+	frameSize int     = 960                 // uint16 size of each audio frame
+	maxBytes  int     = (frameSize * 2) * 2 // max size of opus data
+	volume    float32 = 0.1
 )
 
 func (s *Session) PlayFile(file string) {
 	// Transcode file into PCM for piping to discord
-	run := exec.Command("ffmpeg", "-i", file, "-f", "s16le", "-ar", strconv.Itoa(frameRate), "-ac", strconv.Itoa(channels), "pipe:1")
+	run := exec.Command("ffmpeg", "-i", file, "-af", fmt.Sprintf("volume=%f", volume), "-f", "s16le", "-ar", strconv.Itoa(frameRate), "-ac", strconv.Itoa(channels), "pipe:1")
 
 	ffmpegOut, err := run.StdoutPipe()
 	if err != nil {
@@ -40,6 +42,8 @@ func (s *Session) PlayFile(file string) {
 			log.Println("error closing stdout for ffmpeg,", err)
 		}
 	}(ffmpegOut)
+
+	run.Stderr = os.Stderr
 
 	ffmpegBuf := bufio.NewReaderSize(ffmpegOut, 16384)
 
